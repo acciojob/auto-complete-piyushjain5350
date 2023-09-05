f9edea6671f1ed34e66a6509759183438a6124f1
Piyush Jain <piyushjain5350@gmail.com>
2023-09-05 16:03:00 +0530

    1 soln

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,32 @@
 
-import React from "react";
+import React, { useState ,useEffect} from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const fruits = ["apple", "banana", "cherry", "date", "elderberry", "fig"];
+  const [search, setSearch]=useState('');
+  const [display,setDisplay]=useState([...fruits]);
+
+  useEffect(()=>{
+    if(search.length>0){
+      let filteredData=fruits.filter((item)=>item.includes(search));
+      // console.log(filteredData);
+      setDisplay(filteredData);
+    }else{
+      setDisplay([...fruits]);
+    }
+  },[search])
+  
   return (
     <div>
-        {/* Do not remove the main div */}
+        <input type="text" value={search} onChange={(e)=>setSearch(e.target.value)} />
+        <ul>
+          {
+            display.map(((item,idx)=>(
+              <li key={idx}>{item}</li>
+            )))
+          }
+        </ul>
     </div>
   )
 }
